e4f69a6068de5a33c5f428285237b17940aabd22
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-01 15:44:42 +0800

    Final Submission, Guessing Game

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FECP-Java-Session1-GuessingGame
+# Lab 3: Guessing Game
+Developed by: Baticos, Andric Quinn S.
+
+### Screenshot of Proof
+![Screenshot Proof](/proof.png?raw=true "proof")
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests fixing it.
-		System.out.printf("Hello and welcome!");
 
-		for (int i = 1; i <= 5; i++) {
-			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-			System.out.println("i = " + i);
+		int secretNumber = (int)(Math.random()*5) + 1;
+		int chances = 3, counter=1, userGuess; boolean isCorrect=false;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("I'm thinking of a number between 1 and 5.");
+		while (chances!=0) {
+
+			System.out.print("Guess " + counter++ + ": ");
+			userGuess = in.nextInt();
+
+			if (userGuess==secretNumber) {
+				System.out.println("You guessed it!");
+				isCorrect=true;
+				break;
+			}
+			else System.out.println("Wrong guess.");
+
+			chances--;
 		}
+		if (isCorrect) System.out.println("You win!");
+		else System.out.println("You lose. The correct number was " + secretNumber + ".");
 	}
 }
